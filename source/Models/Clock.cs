e22df9852af6d8cd71b7f4be85a17c2a145bd304
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2021-11-18 06:33:10 +0100

    fix: localized clock

--- source/Models/Clock.cs
+++ source/Models/Clock.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,23 +16,31 @@ namespace LandingPage.Models
 
         public Clock()
         {
+            var hours = Hour;
             dispatcherTimer = new DispatcherTimer(
                 TimeSpan.FromSeconds(60 - (DateTime.Now.TimeOfDay.TotalSeconds % 60) + 0.01),
                 DispatcherPriority.Normal,
                 (sender, args) =>
                 {
                     dispatcherTimer.Interval = TimeSpan.FromSeconds(60);
+                    //var currentCulture = CultureInfo.CurrentCulture;
+                    //CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
                     var currentTime = DateTime.Now;
                     if (currentTime != Time)
                     {
+                        Time = currentTime;
                         if (currentTime.DayOfYear != Time.DayOfYear)
                         {
                             OnDayChanged(EventArgs.Empty);
                         }
-                        Time = currentTime;
                         OnPropertyChanged(nameof(Time));
                         OnPropertyChanged(nameof(DateString));
+                        OnPropertyChanged(nameof(Hour));
+                        OnPropertyChanged(nameof(Minute));
+                        OnPropertyChanged(nameof(Separator));
+                        OnPropertyChanged(nameof(T));
                     }
+                    //CultureInfo.CurrentCulture = currentCulture;
                 },
                 Application.Current.Dispatcher
             );
@@ -45,6 +54,44 @@ namespace LandingPage.Models
         }
 
         public DateTime Time { get; set; } = DateTime.Now;
+
+        public string Hour 
+        { 
+            get
+            {
+                if (!string.IsNullOrEmpty(DateTimeFormatInfo.CurrentInfo.AMDesignator) || !string.IsNullOrEmpty(DateTimeFormatInfo.CurrentInfo.PMDesignator))
+                {
+                    return Time.ToString("hh");
+                } else
+                {
+                    return Time.ToString("HH");
+                }
+            } 
+        }
+
+        public string Minute
+        {
+            get
+            {
+                return Time.ToString("mm");
+            }
+        }
+
+        public string Separator
+        {
+            get
+            {
+                return DateTimeFormatInfo.CurrentInfo.TimeSeparator;
+            }
+        }
+
+        public string T
+        {
+            get
+            {
+                return Time.ToString("tt");
+            }
+        }
 
         public string DateString => Time.ToLongDateString();
 
