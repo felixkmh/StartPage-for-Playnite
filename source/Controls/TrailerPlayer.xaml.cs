fded97922efa4341523a0a414e539fc903103188
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2022-02-01 19:51:41 +0100

    feat: video trailers fade in and out

---
+++ source/Controls/TrailerPlayer.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LandingPage.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für TrailerPlayer.xaml
+    /// </summary>
+    public partial class TrailerPlayer : MediaElement
+    {
+        public TrailerPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            // Source = null;
+        }
+    }
+}
