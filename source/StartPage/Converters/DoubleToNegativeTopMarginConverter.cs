37f01cd2fd2ab8ab2116b0d452ccb383cb86ab70
felixkmh <24227002+felixkmh@users.noreply.github.com>
2022-05-09 14:13:01 +0200

    added converter

---
+++ source/StartPage/Converters/DoubleToNegativeTopMarginConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LandingPage.Converters
+{
+    public class DoubleToNegativeTopMarginConverter : IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new Thickness(0, -System.Convert.ToDouble(value), 0, 0);
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
